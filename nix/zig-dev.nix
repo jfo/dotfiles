a6c8cfdba8a35d94d6dda03a3fd0f1c5760c9b0a
Jeff Fowler <jeffowler@gmail.com>
2019-11-02 21:58:08 +0100

    zig dev update

--- nix/zig-dev.nix
+++ nix/zig-dev.nix
@@ -1,13 +1,19 @@
-with import <nixpkgs> {}; {
+let
+  nixpkgs-src = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/tarball/471869c9185fb610e67940a701eb13b1cfb335a4";
+    sha256 = "1klbclz8n4b9k1kfwv806bqdavld1mg32l1vxsmnrqzr6zck1c54";
+  };
+  nixpkgs = import nixpkgs-src {};
+in with nixpkgs; {
   tmpAoeu = stdenv.mkDerivation {
     name = "tmpAoeu";
 
     hardeningDisable = [ "all" ];
     buildInputs = [
       cmake
-      llvmPackages_8.clang-unwrapped
-      llvm_8
-      lld_8
+      llvmPackages_9.clang-unwrapped
+      llvm_9
+      lld_9
     ];
   };
 }
