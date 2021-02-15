489f3088883640084916528b8150f65f084626f1
Jeff Fowler <jeff.fowler@peakon.com>
2021-02-15 11:49:09 +0100

    fish changes

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+    set -l last_status $status
 
     if not set -q __fish_git_prompt_show_informative_status
         set -g __fish_git_prompt_show_informative_status 1
