add0e296b3ee59196344dc660042aed8c28eb0dc
Jeff Fowler <jeffowler@gmail.com>
2022-07-02 14:34:27 +0200

    nix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -45,15 +45,16 @@ if test -f ~/.config/fish/fish_secrets
 end
 
 set -e fish_user_paths
-set -U fish_user_paths ~/.npm-packages/bin
+set -Ua fish_user_paths ~/bin
+set -Ua fish_user_paths ~/.npm-packages/bin
 set -Ua fish_user_paths /usr/local/opt/postgresql@11/bin
 
-set -gx PATH "/var/folders/0m/w5lp5n515nqfkkl53913y6q00000gp/T/fnm_multishells/35925_1617801194335/bin" $PATH;
-set -gx FNM_MULTISHELL_PATH "/var/folders/0m/w5lp5n515nqfkkl53913y6q00000gp/T/fnm_multishells/35925_1617801194335";
-set -gx FNM_DIR "/Users/jeffrey.fowler/.fnm";
-set -gx FNM_LOGLEVEL "info";
-set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
-set -gx FNM_ARCH "x64";
+# set -gx PATH "/home/jfo/.bin" "/var/folders/0m/w5lp5n515nqfkkl53913y6q00000gp/T/fnm_multishells/35925_1617801194335/bin"  $PATH;
+# set -gx FNM_MULTISHELL_PATH "/var/folders/0m/w5lp5n515nqfkkl53913y6q00000gp/T/fnm_multishells/35925_1617801194335";
+# set -gx FNM_DIR "/Users/jeffrey.fowler/.fnm";
+# set -gx FNM_LOGLEVEL "info";
+# set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+# set -gx FNM_ARCH "x64";
 
 function _fnm_autoload_hook --on-variable PWD --description 'Change Node version on directory change'
     status --is-command-substitution; and return
