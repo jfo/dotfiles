c17c1ed7ff782b9fb515c2d8b5378f5adaa99f2b
Jeff Fowler <jeffowler@gmail.com>
2024-12-16 16:19:15 +0100

    works

--- dots/.config/nvim/init-post.lua
+++ dots/.config/nvim/init-post.lua
@@ -31,9 +31,9 @@ vim.api.nvim_set_keymap('t', '<C-l>', '<C-\\><C-n><C-w>l', {noremap = true})
 
 -- do not jump to next match immediately
 vim.keymap.set('n', '*', function()
-    local word = vim.fn.expand('<cword>')
-    vim.fn.setreg('/', '\\<' .. word .. '\\>')
-    vim.opt.hlsearch = true
+  local word = vim.fn.expand('<cword>')
+  vim.fn.setreg('/', '\\<' .. word .. '\\>')
+  vim.opt.hlsearch = true
 end, { silent = true })
 
 -- LSP client/server setup
