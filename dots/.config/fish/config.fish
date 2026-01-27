8aaa0203aad267a008c40abe2b9229c461c26e11
Jeff Fowler <jeffowler@gmail.com>
2026-01-27 15:00:40 +0100

    asdf

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -82,3 +82,11 @@ end
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 set -x PATH "/opt/miniconda3/bin" $PATH
 
+set _asdf_shims "$HOME/.asdf/shims"
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
